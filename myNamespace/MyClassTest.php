dcea19b7599bbd150c5fb8612b606fda13026d4d
YZ4 <yz4@mail.ru>
2019-06-19 23:45:09 +0300

    test start fix

--- myNamespace/MyClassTest.php
+++ myNamespace/MyClassTest.php
@@ -2,13 +2,14 @@
 
 namespace myNamespace;
 
+use PHPUnit\Framework\TestCase;
 
-class MyClassTest extends \PHPUnit_Framework_TestCase
+class MyClassTest extends TestCase
 {
 
     public function test__construct()
     {
         $myClass = new MyClass("Hello world");
-        $this.assertSame("Hello world", $myClass->parameter);
+        $this->assertSame("Hello world", $myClass->parameter);
     }
 }
